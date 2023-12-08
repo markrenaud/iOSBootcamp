c6ed0f96c04e62e44278dca918ca41da925099eb
Mark Renaud <develop@renaud.com.au>
2023-12-08 16:26:45 +1000

    Initial Commit

---
+++ Module1 Homework/Module1_HomeworkApp.swift
@@ -0,0 +1,16 @@
+//
+//  Module1_HomeworkApp.swift
+//  Created by Mark Renaud (2023).
+//
+    
+
+import SwiftUI
+
+@main
+struct Module1_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
