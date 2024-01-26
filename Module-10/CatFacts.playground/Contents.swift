78a66be1ff1202c19462949d398e2b9698b3079f
Mark Renaud <develop@renaud.com.au>
2024-01-26 10:50:22 +1000

    Refactored: make Service a protocol; replicate hasNext() functionality in iterator.

--- Module-10/CatFacts.playground/Contents.swift
+++ Module-10/CatFacts.playground/Contents.swift
@@ -2,101 +2,74 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-// 📌 Requirement:
-// ✅ A struct for coding the JSON response from https://catfact.ninja/fact
-
 struct CatFact: Codable {
     let fact: String
     let length: Int
 }
 
-struct CatFactService {
-    
-    static let defaultEndpoint = URL(string: "https://catfact.ninja/fact")!
-    
-    /// The API endpoint for Cat Facts
-    let endpoint: URL
-    
-    init(endpoint: URL = defaultEndpoint) {
-        self.endpoint = endpoint
+protocol CatFactService {
+    func retrieveFact() async throws -> CatFact
+    func logFacts(maxFacts: UInt) async throws
+}
+
+extension CatFactService {
+    func logFacts(maxFacts: UInt) async throws {
+        var count = 0
+        let sequence = CatFactSequence(service: self) {
+            count += 1
+            return count <= maxFacts
+        }
+        for try await catFact in sequence {
+            print(catFact.fact)
+        }
     }
-    
+}
+
+struct CatFactNinja: CatFactService {
     /// Retrieves a random CatFact from catfact.ninja using URLSession.shared.
     func retrieveFact() async throws -> CatFact {
+        let endpoint = URL(string: "https://catfact.ninja/fact")!
         let (data, _) = try await URLSession.shared.data(from: endpoint)
         return try JSONDecoder().decode(CatFact.self, from: data)
     }
-    
 }
-
-// 📌 Requirement:
-// ✅ A struct for the AsyncSequence
 
 struct CatFactSequence: AsyncSequence {
     typealias Element = CatFact
-    
     /// The CatFactService to use
     let service: CatFactService
-    /// The maximum number of cat facts to be emitted before terminating the sequence.
-    let maxFacts: UInt
     
-    init(maxFacts: UInt, using service: CatFactService = CatFactService()) {
+    let hasNext: () -> Bool
+
+    init(service: CatFactService, hasNext: @escaping () -> Bool = { true }) {
         self.service = service
-        self.maxFacts = maxFacts
-    }
-    
-    // 📌 Requirement:
-    // ✅ A struct for the Iterator
-    
-    struct CatFactIterator: AsyncIteratorProtocol {
-        /// The maximum number of cat facts to be emitted before terminating the sequence.
-        let maxFacts: UInt
-        /// The CatFactService to use
-        let service: CatFactService
-        
-        private var factsReturned = 0
-        
-        init(maxFacts: UInt, service: CatFactService) {
-            self.maxFacts = maxFacts
-            self.service = service
-        }
-        
-        mutating func next() async throws -> CatFact? {
-            // double-check task has not been cancelled - throw if it has
-            try Task.checkCancellation()
-            // Check if max cat facts has been reached.
-            // If max cat facts have been emitted, end the sequence
-            // by returning nil
-            guard factsReturned < maxFacts else { return nil }
-            // retrieve a random cat fact
-            let fact = try await service.retrieveFact()
-            // if we have successfully retrieved, increment
-            // counter and return the fact to the sequence
-            factsReturned += 1
-            return fact
-        }
+        self.hasNext = hasNext
     }
     
     func makeAsyncIterator() -> CatFactIterator {
-        return CatFactIterator(maxFacts: maxFacts, service: service)
+        return CatFactIterator(service: service, hasNext: hasNext)
     }
 }
 
-// 📌 Requirement:
-// ✅ A function that uses the iterator to get Cat Facts.
-// ✅ The function must have a parameter that allows the user to set the number of Cat Facts to get.
+struct CatFactIterator: AsyncIteratorProtocol {
+    /// The CatFactService to use
+    let service: CatFactService
+    
+    let hasNext: () -> Bool
 
-/// Prints a specified number of cat facts to the console.
-func logCatFacts(count: UInt) {
-    Task {
-        do {
-            for try await catFact in CatFactSequence(maxFacts: count) {
-                print(catFact.fact)
-            }
-        } catch {
-            print("An error occured retrieving cat facts:", error.localizedDescription)
-        }
+    mutating func next() async throws -> CatFact? {
+        // double-check task has not been cancelled - throw if it has
+        try Task.checkCancellation()
+        // confirm we should continue against any passed in predicate
+        // return nil to end sequence if iterator should not continue.
+        if !hasNext() { return nil }
+        // retrieve a random cat fact
+        let fact = try await service.retrieveFact()
+        return fact
     }
 }
 
-logCatFacts(count: 5)
+Task {
+    let service = CatFactNinja()
+    try await service.logFacts(maxFacts: 5)
+}
